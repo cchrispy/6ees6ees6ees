ff0c29f5ff08d493a90b282f72647c602a8a9ea1
Chris Chiang <cchiang1992@gmail.com>
2016-08-07 16:24:41 -0700

    passed all tests

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,7 +1,17 @@
-import Bee from './Bee'
+import HoneyMakerBee from './HoneyMakerBee'
 
-class ForagerBee {
+class ForagerBee extends HoneyMakerBee {
   // TODO..
+  constructor() {
+    super();
+    this.age = 10;
+    this.job = 'find pollen';
+    this.canFly = true;
+    this.treasureChest = [];
+  }
+  forage() {
+    this.treasureChest.push('treasure');
+  }
 };
 
 export default ForagerBee;
